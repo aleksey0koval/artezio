65d5b584fbe842d55ee26b24fae91506bb9256bc
akovalchuk <aleksey0koval@gmail.com>
2021-06-29 14:53:44 +0300

    Покрыл Unit тестами методы калькулятора используя JUnit5 библиотеку в Maven проекте

---
+++ JUnit5Maven/src/main/java/com/artezio/JUnit5Maven/CalculatorService.java
@@ -0,0 +1,22 @@
+package com.artezio.JUnit5Maven;
+
+public class CalculatorService {
+    public double sum(int a, int b) {
+        return a + b;
+    }
+
+    public double minus(int a, int b) {
+        return a - b;
+    }
+
+    public double multiple(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+        return a / b;
+    }
+}
---
+++ JUnit5Maven/src/test/java/com/artezio/JUnit5Maven/CalculatorServiceTest.java
@@ -0,0 +1,50 @@
+package com.artezio.JUnit5Maven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class CalculatorServiceTest {
+
+    @Test
+    void sum() {
+        CalculatorService calculator = new CalculatorService();
+        double result = calculator.sum(1, 2);
+        System.out.println(result);
+        if (result != 3) {
+            fail();
+        }
+    }
+
+    @Test
+    void minus() {
+        CalculatorService calculator = new CalculatorService();
+        double result = calculator.minus(5, 2);
+        System.out.println(result);
+        assertEquals(3, result);
+    }
+
+    @Test
+    void multiple() {
+        CalculatorService calculator = new CalculatorService();
+        double result = calculator.multiple(2, 2);
+        System.out.println(result);
+        assertEquals(4, result);
+    }
+
+    @Test
+    void divide() {
+        CalculatorService calculator = new CalculatorService();
+        double result = calculator.divide(2, 2);
+        System.out.println(result);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testDivideWillThrowExceptionWhenDivideOnZero() {
+        CalculatorService calculator = new CalculatorService();
+        double result = calculator.divide(6, 0);
+        System.out.println(result);
+    }
+}
